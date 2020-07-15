8b1b3624f5840044faa0b7067a1d28f8bf545bbf
Jetmir Avdullahu <jetmiravdullahu@Jetmirs-iMac.local>
2020-07-15 13:57:21 -0700

    get currencies from open api

--- CryptoTracker/Controllers/CryptoTableViewController.swift
+++ CryptoTracker/Controllers/CryptoTableViewController.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+import UIKit
+
+struct Country {
+    // ...
+}
+
+class CountryTableViewCell: UITableViewCell {
+}
+
+class CountriesTableViewController: UITableViewController {
+    // ...
+}
